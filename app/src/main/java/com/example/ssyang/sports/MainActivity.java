a6293748db1bbc1e0c083c4934a53e43dc93e67a
syangtm <42742697+syangtm@users.noreply.github.com>
2018-10-19 10:10:58 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/ssyang/sports/MainActivity.java
+++ app/src/main/java/com/example/ssyang/sports/MainActivity.java
@@ -20,26 +20,30 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
 
-    public void incrementA(View view){
+     public void incrementA(View view){
         TextView quantityTextView = (TextView) findViewById(R.id.ascore);
         int value = Integer.parseInt((String)(quantityTextView + " "));
         quantityTextView.setText(pointA+1+"");
+        pointA+=1;
     }
 
     public void incrementB(View view){
         TextView quantityTextView = (TextView) findViewById(R.id.bscore);
         int value = Integer.parseInt((String)(quantityTextView + " "));
         quantityTextView.setText(pointB+1+"");
+        pointB+=1;
     }
 
     public void decrementA(View view){
         TextView quantityTextView = (TextView) findViewById(R.id.ascore);
         int value = Integer.parseInt((String)(quantityTextView + " "));
         quantityTextView.setText(pointA-1+"");
+        pointA-=1;
     }
     public void decrementB(View view){
         TextView quantityTextView = (TextView) findViewById(R.id.ascore);
         int value = Integer.parseInt((String)(quantityTextView + " "));
         quantityTextView.setText(pointB-1+"");
+        pointB-=1;
     }
 }
