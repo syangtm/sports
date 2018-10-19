fd5ac956597dc8b6aee74c371dd0fcaa49a8ed70
Yang <stephanie.yang2@stu.fayette.kyschools.us>
2018-10-19 10:05:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ssyang/sports/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ssyang.sports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int pointA =0;
+    int pointB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+
+    public void incrementA(View view){
+        TextView quantityTextView = (TextView) findViewById(R.id.ascore);
+        int value = Integer.parseInt((String)(quantityTextView + " "));
+        quantityTextView.setText(pointA+1+"");
+    }
+
+    public void incrementB(View view){
+        TextView quantityTextView = (TextView) findViewById(R.id.bscore);
+        int value = Integer.parseInt((String)(quantityTextView + " "));
+        quantityTextView.setText(pointB+1+"");
+    }
+
+    public void decrementA(View view){
+        TextView quantityTextView = (TextView) findViewById(R.id.ascore);
+        int value = Integer.parseInt((String)(quantityTextView + " "));
+        quantityTextView.setText(pointA-1+"");
+    }
+    public void decrementB(View view){
+        TextView quantityTextView = (TextView) findViewById(R.id.ascore);
+        int value = Integer.parseInt((String)(quantityTextView + " "));
+        quantityTextView.setText(pointB-1+"");
+    }
+}
